ce56ef301f5cc7e6efb50f82d2eddf5a424aa380
shen wenjie <970504888@qq.com>
2026-09-06 18:27:07 +0800

    build: install SDK feeds before APK compile

--- tests/test_package_manifest.sh
+++ tests/test_package_manifest.sh
@@ -7,3 +7,4 @@ test -f package/doorfast/files/doorfast.config
 test -f scripts/prepare-sdk-package.sh
 grep -q 'PKGARCH:=x86_64' package/doorfast/Makefile
 ! grep -R -E -q 'wget -O-|auth|auto_update|opkg|\.ipk' package/doorfast scripts
+grep -q 'scripts/feeds install libpcap libuci libjson-c libopenssl' .github/workflows/build-apk.yml
