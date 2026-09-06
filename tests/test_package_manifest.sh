733c2e866d37e2ba7e9ad94c703b1c971cf5bd7f
shen wenjie <970504888@qq.com>
2026-09-06 18:20:43 +0800

    test: remove ripgrep dependency from package check

--- tests/test_package_manifest.sh
+++ tests/test_package_manifest.sh
@@ -5,5 +5,5 @@ test -f package/doorfast/Makefile
 test -f package/doorfast/files/doorfast.init
 test -f package/doorfast/files/doorfast.config
 test -f scripts/prepare-sdk-package.sh
-rg -q 'PKGARCH:=x86_64' package/doorfast/Makefile
-! rg -q 'wget -O-|auth|auto_update|opkg|\.ipk' package/doorfast scripts
+grep -q 'PKGARCH:=x86_64' package/doorfast/Makefile
+! grep -R -E -q 'wget -O-|auth|auto_update|opkg|\.ipk' package/doorfast scripts
