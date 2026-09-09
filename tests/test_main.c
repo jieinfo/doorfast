76eac251a0bc6cd5ddbdf13e77978784e65216b4
shen wenjie <970504888@qq.com>
2026-09-09 17:45:38 +0800

    feat: dispatch offline call commands with bounded retries

--- tests/test_main.c
+++ tests/test_main.c
@@ -91,8 +91,10 @@ void test_audit_format_is_redacted(void);
 void test_discovery_requires_approval(void);
 void test_network_overlap_is_read_only(void);
 
+void test_gvs_call_dispatch_lifecycle(void);
+void test_gvs_call_dispatch_bounds(void);
 int test_suite_count(void) {
-    return 88;
+    return 90;
 }
 
 int main(void) {
@@ -101,7 +103,9 @@ int main(void) {
     test_gvs_session_preemption_transaction();
     test_gvs_priority_valid_matrix_and_unknown_categories();
     test_gvs_pick_exchange();
-    TEST_ASSERT_INT_EQ(88, test_suite_count());
+    TEST_ASSERT_INT_EQ(90, test_suite_count());
+    test_gvs_call_dispatch_lifecycle();
+    test_gvs_call_dispatch_bounds();
     test_gvs_peer_sim_rejects_invalid_time_and_bounds_queue();
     test_gvs_peer_sim_builds_synthetic_call_frame();
     test_gvs_peer_sim_elects_maintainer_without_peers();
